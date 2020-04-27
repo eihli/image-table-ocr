449ee015d3019307b0e8adcab7f0dcbe1e4f1838
Eric Ihli <ericihli@gmail.com>
2020-04-27 11:47:48 -0700

    Update license and setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import setuptools
 long_description = """
 Utilities for turning images of tables into CSV data. Uses Tesseract and OpenCV.
 
-Requires binaries for tesseract and pdfimages (from Poppler).
+Requires binaries for tesseract, ImageMagick, and pdfimages (from Poppler).
 """
 setuptools.setup(
     name="table_ocr",
-    version="0.0.1",
+    version="0.1",
     author="Eric Ihli",
     author_email="eihli@owoga.com",
-    description="Turn images of tables into CSV data.",
+    description="Extract text from tables in images.",
     long_description=long_description,
     long_description_content_type="text/plain",
     url="https://github.com/eihli/image-table-ocr",
@@ -23,6 +23,7 @@ setuptools.setup(
     install_requires=[
         "pytesseract~=0.3",
         "opencv-python~=4.2",
+        "numpy~=1.18.1",
     ],
     python_requires='>=3.6',
 )
